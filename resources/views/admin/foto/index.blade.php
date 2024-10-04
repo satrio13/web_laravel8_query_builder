c44c59ef2a46e2d47b3dbdf05a96bf04a954ef7e
satrioed <32364936+satrio13@users.noreply.github.com>
2024-10-04 16:44:13 +0700

    Update index.blade.php

--- resources/views/admin/foto/index.blade.php
+++ resources/views/admin/foto/index.blade.php
@@ -5,7 +5,7 @@
     <div class="container-fluid">
       <div class="row mb-2">
         <div class="col-sm-6">
-          <h1 class="m-0 text-dark"><?= $title; ?></h1>
+          <h1 class="m-0 text-dark">{{ $title }}</h1>
         </div>
         <div class="col-sm-6">
           <ol class="breadcrumb float-sm-right">
@@ -27,7 +27,7 @@
             @endif
             <div class="card card-primary">
                 <div class="card-header">
-                    <h3 class="card-title">FORM <?= strtoupper($title); ?></h3>
+                    <h3 class="card-title">FORM {{ strtoupper($title) }}</h3>
                 </div>
                 <form method="POST" action="{{ route('backend/simpan-foto') }}" enctype="multipart/form-data" id="form">
                     @csrf
