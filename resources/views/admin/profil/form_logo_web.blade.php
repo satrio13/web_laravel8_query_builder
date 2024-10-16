78139e0b9c0ebfef856f9f4cbc1e299a4ec7ff91
satrioed <32364936+satrio13@users.noreply.github.com>
2024-10-16 23:47:08 +0700

    Update form_logo_web.blade.php

--- resources/views/admin/profil/form_logo_web.blade.php
+++ resources/views/admin/profil/form_logo_web.blade.php
@@ -38,7 +38,7 @@
                                         @if(empty($data->logo_web))
                                             <img class='img-responsive' id='preview_gambar' width='150px'>
                                         @else
-                                            Logo Sekarang: <img class='img-responsive mb-2' id='preview_gambar' width='150px' src="/img/logo/{{ $data->logo_web }}">
+                                            <img class='img-responsive mb-2' id='preview_gambar' width='150px' src="/img/logo/{{ $data->logo_web }}">
                                         @endif
                                         <input type='file' name='logo_web' id="file-upload" accept='image/png, image/jpeg' class='form-control' onchange='readURL(this);' required>
                                         <p style="color: red"> *) format file JPG/PNG ukuran maksimal 1 MB</p>
