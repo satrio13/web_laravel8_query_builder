68a8161bff62cd9b286bc45f17fe2da5154f6aa4
satrioed <32364936+satrio13@users.noreply.github.com>
2024-10-16 09:36:35 +0700

    Update PrestasiGuruController.php

--- app/Http/Controllers/admin/PrestasiGuruController.php
+++ app/Http/Controllers/admin/PrestasiGuruController.php
@@ -42,6 +42,10 @@ class PrestasiGuruController extends Controller
             'tingkat' => 'required|numeric',
             'keterangan' => 'max:100',
             'gambar' => 'image|mimes:jpeg,jpg,png|max:1024'
+        ],
+        [
+            'nama.required' => 'Kolom nama lomba harus diisi.',
+            'nama.max:100' => 'Kolom nama lomba harus kurang dari atau sama dengan :value karakter.'
         ]);
             
         $nama_gambar = '';
@@ -101,6 +105,10 @@ class PrestasiGuruController extends Controller
             'tingkat' => 'required|numeric',
             'keterangan' => 'max:100',
             'gambar' => 'image|mimes:jpeg,jpg,png|max:1024'
+        ],
+        [
+            'nama.required' => 'Kolom nama lomba harus diisi.',
+            'nama.max:100' => 'Kolom nama lomba harus kurang dari atau sama dengan :value karakter.'
         ]);
             
         $get = $this->prestasi_guru_model->cek_prestasi_guru($id);
