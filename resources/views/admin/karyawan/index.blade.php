06fc98951b54cbb6f733ce78b40e1185cf95bc22
satrioed <32364936+satrio13@users.noreply.github.com>
2024-10-19 12:19:26 +0700

    Update index.blade.php

--- resources/views/admin/karyawan/index.blade.php
+++ resources/views/admin/karyawan/index.blade.php
@@ -289,7 +289,7 @@
                         var fileUrl = base_url +'/img/karyawan/'+ data.gambar;
                         check_file_exists(fileUrl, function(exists)
                         {
-                            if(data.gambar != '')
+                            if(data.gambar !== '')
                             {
                                 $("#img").html('<img src="'+ base_url +'/img/karyawan/'+ data.gambar +'" class="img img-fluid img-thumbnail" width="120px">');
                             }else
@@ -298,9 +298,9 @@
                             }
                         });
 
-                        var tgl_lahir = (data.tgl_lahir != '0000-00-00' && data.tgl_lahir !== null) ? tgl_indo(data.tgl_lahir) : '';
-                        var tmt_cpns = (data.tmt_cpns != '0000-00-00' && data.tmt_cpns !== null) ? tgl_indo(data.tmt_cpns) : '';
-                        var tmt_pns = (data.tmt_pns != '0000-00-00' && data.tmt_pns !== null) ? tgl_indo(data.tmt_pns) : '';
+                        var tgl_lahir = (data.tgl_lahir !== '0000-00-00' && data.tgl_lahir !== null) ? tgl_indo(data.tgl_lahir) : '';
+                        var tmt_cpns = (data.tmt_cpns !== '0000-00-00' && data.tmt_cpns !== null) ? tgl_indo(data.tmt_cpns) : '';
+                        var tmt_pns = (data.tmt_pns !== '0000-00-00' && data.tmt_pns !== null) ? tgl_indo(data.tmt_pns) : '';
 
                         var jk;
                         if(data.jk == 1)
