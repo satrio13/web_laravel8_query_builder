ab4ab7f2061f51d89b3fcf80c5038fbf5ceb9d4a
satrioed <32364936+satrio13@users.noreply.github.com>
2024-10-19 12:19:56 +0700

    Update index.blade.php

--- resources/views/admin/guru/index.blade.php
+++ resources/views/admin/guru/index.blade.php
@@ -288,7 +288,7 @@
                         var fileUrl = base_url +'/img/guru/'+ data.gambar;
                         check_file_exists(fileUrl, function(exists)
                         {
-                            if(data.gambar != '')
+                            if(data.gambar !== '')
                             {
                                 $("#img").html('<img src="'+ base_url +'/img/guru/'+ data.gambar +'" class="img img-fluid img-thumbnail" width="120px">');
                             }else
@@ -297,10 +297,10 @@
                             }
                         });
 
-                        var tgl_lahir = (data.tgl_lahir != '0000-00-00' && data.tgl_lahir !== null) ? tgl_indo(data.tgl_lahir) : '';
-                        var tmt_cpns = (data.tmt_cpns != '0000-00-00' && data.tmt_cpns !== null) ? tgl_indo(data.tmt_cpns) : '';
-                        var tmt_pns = (data.tmt_pns != '0000-00-00' && data.tmt_pns !== null) ? tgl_indo(data.tmt_pns) : '';
-                        
+                        var tgl_lahir = (data.tgl_lahir !== '0000-00-00' && data.tgl_lahir !== null) ? tgl_indo(data.tgl_lahir) : '';
+                        var tmt_cpns = (data.tmt_cpns !== '0000-00-00' && data.tmt_cpns !== null) ? tgl_indo(data.tmt_cpns) : '';
+                        var tmt_pns = (data.tmt_pns !== '0000-00-00' && data.tmt_pns !== null) ? tgl_indo(data.tmt_pns) : '';
+
                         var jk;
                         if(data.jk == 1)
                         {
