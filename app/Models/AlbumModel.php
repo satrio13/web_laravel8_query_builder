068d324cc8b88352514a09c60bbb044ccf26a16c
satrioed <32364936+satrio13@users.noreply.github.com>
2024-11-02 13:35:26 +0700

    Update AlbumModel.php

--- app/Models/AlbumModel.php
+++ app/Models/AlbumModel.php
@@ -10,7 +10,7 @@ class AlbumModel
 
     function list_album($page)
     {
-        return DB::table($this->table)->where('is_active', 1)->orderBy('updated_at','desc')->paginate($page);
+        return DB::table($this->table)->where('is_active', 1)->orderBy('created_at','desc')->paginate($page);
     }
 
     function cek_album($slug)
@@ -25,7 +25,7 @@ class AlbumModel
 
     function list_foto($slug)
     {
-        return DB::table('tb_foto')->select('tb_foto.*', 'tb_album.*')->join('tb_album','tb_foto.id_album', '=', 'tb_album.id_album')->where('tb_album.is_active',1)->where('tb_album.slug',$slug)->orderBy('tb_foto.updated_at','desc')->get();
+        return DB::table('tb_foto')->select('tb_foto.*', 'tb_album.*')->join('tb_album','tb_foto.id_album', '=', 'tb_album.id_album')->where('tb_album.is_active',1)->where('tb_album.slug', $slug)->orderBy('tb_foto.created_at','desc')->get();
     }
 
 }
